1c071751c45a35f260a2295ccb4b62c1e774e752
Daisy Zhang <daisyzhang@usc-guestwireless-upc-new115.usc.edu>
2022-02-20 01:05:53 -0800

    updating website

--- templates/static/app.js
+++ templates/static/app.js
@@ -8,23 +8,58 @@ d3.json("../tweet.json").then(function(tweetdata){
     // adding ids to the dropdown menu
     for(let i =0; i< names.length; i++){
         selectBox.append("option").text(Object.keys(values[i]));
-        console.log(Object.keys(values[i]));
+        //console.log(Object.keys(values[i]));
     }
-    let info = data[1][1].data
-    console.log(Object.values(info[1].public_metrics))
+
+    updateinfo(0)
+
+    function updateinfo(id){
+        let info = data[1][id].data
+        let likeCount = 0
+        for(let i = 0 ; i < info.length; i++){
+            likeCount = likeCount + info[i].public_metrics.like_count
+        }
+
+        let quoteCount = 0
+        for(let i = 0 ; i < info.length; i++){
+            quoteCount =  quoteCount + info[i].public_metrics.quote_count
+        }
+
+        let replyCount = 0
+        for(let i = 0 ; i < info.length; i++){
+            replyCount =  replyCount + info[i].public_metrics.reply_count
+        }
+
+        let retweetCount = 0
+        for(let i = 0 ; i < info.length; i++){
+            retweetCount =  retweetCount + info[i].public_metrics.retweet_count
+        }
     
-   updatechart[1]
-
-    function updatechart(id){
-        let info = data[1][id].data;
-        let keys = Object.keys(info[1].public_metrics)
-        let values = Object.values(info[1].public_metrics)
+        console.log(info[1])
+    
         let sample_metadata = d3.select("#sample-metadata");
-
+    
         sample_metadata.html("");
+        sample_metadata.append("li").text("Average Like Count: " + likeCount/100);
+        sample_metadata.append("li").text("Average Quote Count: " + quoteCount/100);
+        sample_metadata.append("li").text("Average Reply Count: " + replyCount/100);
+        sample_metadata.append("li").text("Average Retweet Count: " + retweetCount/100);
 
-        for(let i =0; i< keys.length; i++){
-            sample_metadata.append("p").text(keys[i] + ": " + values[i]);
-        }  
+    }
+
+    d3.selectAll("#selDataset").on("change", updateData);
+
+    function updateData(){
+        let dropdownMenu = d3.select("#selDataset");
+        // Assign the value of the dropdown menu option to a variable
+        let dataset = dropdownMenu.property("value");
+        console.log("value", dataset);
+        console.log(Object.keys(Object.values(data[0])[1])[0]);
+
+        for(let i =0; i< data[0].length; i++){
+            if(dataset === Object.keys(Object.values(data[0])[i])[0]){
+                updateinfo(i);
+            }
+        }
     }
 });
